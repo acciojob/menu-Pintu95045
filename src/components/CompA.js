4cc2eea82aa2bff02ce6a8b82fab424ba419845a
Pintu Giri <mrpintugiri@gmail.com>
2024-10-12 16:40:58 +0530

    solution added

--- src/components/CompA.js
+++ src/components/CompA.js
@@ -3,7 +3,7 @@ import React from 'react'
 const CompA = ({item}) => {
 
     const{title, price, desc, category, img} = item
-    // console.log(title)
+    
 
   return (
     <div data-test-id={"menu-item-" + category} style={{display:"flex", border:"1px solid black", width:"500px"}}>
